767590788ad19f144067660754a63d63c4bf97f3
Tomas Gomez <tomasg1223@outlook.com>
2024-06-24 22:02:51 -0300

    desabilito nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://T0MASG0MEZ.github.io/COZMO',
-  base: '/COZMO/',
+  base: '/mi-repo/',
 });
