dbbd011881254c9107dd43600b43002a36e6a3f3
Tomas Gomez <tomasg1223@outlook.com>
2024-06-24 21:54:08 -0300

    prueba deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://T0MASG0MEZ.github.io/COZMO',
+  base: '/COZMO/',
 });
